68bd659e1e1cf7f918dc01f913d9a66b82c2c242
haoranwen0 <hranwen@mit.edu>
2023-11-23 22:18:07 -0500

    began to add diagram react-flow

--- client/src/components/Diagram.tsx
+++ client/src/components/Diagram.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useCallback } from "react";
+import { Box } from "@mui/material";
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  OnConnect,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 const Diagram = () => {
-  return <div></div>;
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect: OnConnect = useCallback(
+    (params) => {
+      console.log(params);
+      setEdges((eds) => addEdge(params, eds));
+    },
+    [setEdges]
+  );
+
+  return (
+    <Box className="flex-1 h-full py-4 pr-4">
+      <Box className="bg-slate-50 w-full h-full rounded-lg">
+        <Box className="w-full h-full">
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Diagram;
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Chat } from "../components";
+import { Chat, Diagram } from "../components";
 
 const Main = () => {
   return (
-    <div className="w-full h-screen bg-slate-800">
+    <div className="w-full h-screen bg-slate-800 flex">
       <Chat />
+      <Diagram />
     </div>
   );
 };
