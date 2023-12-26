fa070c90dd4ece1112fc1910a10fc1517462572c
Hassan Mohiuddin <hassan_m@mit.edu>
2023-12-26 11:51:41 -0500

    canvas diagram

--- client/src/components/Diagram/index.tsx
+++ client/src/components/Diagram/index.tsx
@@ -1,5 +1,5 @@
-import { useCallback } from "react"
-import { Box } from "@mui/material"
+import { useCallback } from "react";
+import { Box, Input, InputAdornment } from "@mui/material";
 import ReactFlow, {
   useNodesState,
   useEdgesState,
@@ -9,14 +9,20 @@ import ReactFlow, {
   Background,
   Controls,
   Connection,
-} from "reactflow"
-import CustomNode from "../react-flow-nodes/CustomeNode"
+  MarkerType,
+  EdgeProps,
+  Edge,
+} from "reactflow";
+import CustomNode from "../react-flow-nodes/CustomeNode";
+import TextNode from "../react-flow-nodes/TextNode";
+import { useState, useRef } from "react";
 
-import "reactflow/dist/style.css"
+import "reactflow/dist/style.css";
 
 const nodeTypes = {
   CustomNode: CustomNode,
-}
+  TextNode: TextNode,
+};
 
 const initialNodes = [
   {
@@ -37,24 +43,156 @@ const initialNodes = [
       icon: "https://static-00.iconduck.com/assets.00/aws-s3-simple-storage-service-icon-423x512-sofvbo3x.png",
     },
   },
-]
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }]
+  {
+    id: "3",
+    position: { x: 0, y: 10 },
+    type: "TextNode",
+    data: {
+      label: "",
+      icon: "",
+    },
+  },
+];
+// const initialEdges = [{ id: "e1-2", source: "1", target: "2" }]
 
 const Diagram = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [fontSize, setFontSize] = useState<number>(16); // Initial font size
+  const [selectedIcon, setSelectedIcon] = useState<string>("");
+  const [selectedShape, setSelectedShape] = useState<string>("");
+  const [selectedFontType, setSelectedFontType] = useState<string>("");
+
+  const handleFontSizeChange = (increment: number) => {
+    setFontSize((prevSize) => prevSize + increment);
+  };
+
+  const handleIconSelection = (value: string) => {
+    setSelectedIcon(value);
+    const icon = `/Users/hassanmohiuddin/fluffy-octo-enigma/client/src/components/images/${value}.png`;
+    console.log(icon);
+    if (value !== "") {
+      const newNode = {
+        id: (nodes.length + 1).toString(),
+        type: "CustomNode",
+        position: { x: 0, y: 0 },
+        data: {
+          label: value,
+          icon: `/Users/hassanmohiuddin/fluffy-octo-enigma/client/src/components/images/loadbalancer.png`,
+        },
+      };
+      setNodes((prevNodes) => [...prevNodes, newNode]);
+    }
+  };
+
+  const handleShapeSelection = (value: string) => {
+    setSelectedShape(value);
+    if (value !== "") {
+      const newNode = {
+        id: (nodes.length + 1).toString(),
+        type: "default",
+        position: { x: 0, y: 0 },
+        data: { label: "", icon: "" },
+      };
+      setNodes((prevNodes) => [...prevNodes, newNode]);
+    }
+  };
+
+  const handleFontTypeSelection = (value: string) => {
+    setSelectedFontType(value);
+  };
 
   const onConnect: OnConnect = useCallback(
     (params: Connection) => {
-      setEdges((eds) => addEdge(params, eds))
+      setEdges((eds) => addEdge(params, eds));
     },
     [setEdges]
-  )
+  );
+
+  const defaultEdgeOptions = {
+    style: { strokeWidth: 3, stroke: "black" },
+    type: "floating",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black",
+    },
+  };
 
   return (
     <Box className="flex-1 h-full py-4 pr-4">
       <Box className="bg-slate-50 w-full h-full rounded-lg">
         <Box className="w-full h-full">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: "16px",
+            }}
+          >
+            {/* Dropdown for node selection */}
+            <select
+              value={selectedIcon}
+              onChange={(e) => handleIconSelection(e.target.value)}
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                padding: "8px",
+              }}
+            >
+              <option value="" disabled hidden>
+                Icons
+              </option>
+              <option value=""></option>
+              <option value="aws-db">AWS DB</option>
+              <option value="server">Server</option>
+              <option value="user">User</option>
+              <option value="loadbalancer">Load Balancer</option>
+            </select>
+
+            {/* Dropdown for shape selection */}
+            <select
+              value={selectedShape}
+              onChange={(e) => handleShapeSelection(e.target.value)}
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                padding: "8px",
+              }}
+            >
+              <option value="" disabled hidden>
+                Shapes
+              </option>
+              <option value=""></option>
+              <option value="rectangle">Rectangle</option>
+              <option value="circle">Circle</option>
+              <option value="square">Square</option>
+            </select>
+
+            {/* Dropdown for font type selection */}
+            <select
+              value={selectedFontType}
+              onChange={(e) => handleFontTypeSelection(e.target.value)}
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                padding: "8px",
+              }}
+            >
+              <option value="" disabled hidden>
+                Fonts
+              </option>
+              <option value=""></option>
+              <option value="Times New Roman">Times New Roman</option>
+              <option value="Arial">Arial</option>
+            </select>
+
+            {/* Font displayer with +/- buttons */}
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <button onClick={() => handleFontSizeChange(1)}>+</button>
+              <div style={{ margin: "0 8px" }}>Font Size: {fontSize}</div>
+              <button onClick={() => handleFontSizeChange(-1)}>-</button>
+            </div>
+          </div>
           <ReactFlow
             nodes={nodes}
             edges={edges}
@@ -62,6 +200,8 @@ const Diagram = () => {
             onEdgesChange={onEdgesChange}
             onConnect={onConnect}
             nodeTypes={nodeTypes}
+            defaultEdgeOptions={defaultEdgeOptions}
+            style={{ fontSize: `${fontSize}px` }}
           >
             <MiniMap
               style={{
@@ -71,15 +211,12 @@ const Diagram = () => {
               pannable
             />
             <Controls />
-            <Background
-              color="#aaa"
-              gap={16}
-            />
+            <Background color="#aaa" gap={16} />
           </ReactFlow>
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Diagram
+export default Diagram;
