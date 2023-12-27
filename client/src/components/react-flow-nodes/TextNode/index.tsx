cb95fbc512c60275fc04ba0fa78def5dade1caa9
haoranwen0 <hranwen@mit.edu>
2023-12-27 14:58:52 -0500

    asasdasdasd

--- client/src/components/react-flow-nodes/TextNode/index.tsx
+++ client/src/components/react-flow-nodes/TextNode/index.tsx
@@ -1,35 +1,35 @@
-import React, { useState, memo } from "react";
-import { GenericService } from "../../../interfaces";
+import React, { useState, memo } from 'react'
+import { GenericService } from '../../../interfaces'
 
 interface CustomNodeProps {
-  data: GenericService;
+  data: GenericService
 }
 
 function TextNode(props: CustomNodeProps) {
-  const [label, setLabel] = useState(props.data.label || "Write here");
+  const [label, setLabel] = useState(props.data.label || 'Write here')
 
   const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLabel(event.target.value);
-  };
+    setLabel(event.target.value)
+  }
 
   return (
     <div
       style={{
-        position: "relative",
-        width: "200px",
-        height: "100px",
-        padding: "10px",
-        border: "1px solid #ddd",
+        position: 'relative',
+        width: '200px',
+        height: '100px',
+        padding: '10px',
+        border: '1px solid #ddd',
       }}
     >
       <input
-        type="text"
+        type='text'
         value={label}
         onChange={handleTextChange}
-        style={{ width: "100%", marginBottom: "10px" }}
+        style={{ width: '100%', marginBottom: '10px' }}
       />
     </div>
-  );
+  )
 }
 
-export default memo(TextNode);
+export default memo(TextNode)
